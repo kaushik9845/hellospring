ba735c16749f98c5e4abeeef6e36078967eaa347
kaushik9845 <kaushik9845@gmail.com>
2019-05-04 17:06:08 +0530

    Update Application.java
    loggers

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.apache.log4j.Logger;
+
+
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    static Logger log = Logger.getLogger(Application.class.getName()); 
     public static void main(String[] args) {
+        log.info("Inside main method1");
         SpringApplication.run(Application.class, args);
+          log.info("Inside main method2");
     }
     
     @Override
