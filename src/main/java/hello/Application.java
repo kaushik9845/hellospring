03f4154469eaaeece2044fc0728346927de278a7
kaushik9845 <kaushik9845@gmail.com>
2019-05-04 17:09:09 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    static Logger log = Logger.getLogger(Application.class.getName()); 
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+   
     public static void main(String[] args) {
         log.info("Inside main method1");
         SpringApplication.run(Application.class, args);
